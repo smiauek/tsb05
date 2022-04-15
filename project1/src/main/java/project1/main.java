7a3250d2befdd1591b091936d471b4e8eeb87713
Piotr Smialkowski <smiaucius@gmail.com>
2022-04-15 14:00:53 -0500

    java day 1 class

---
+++ project1/src/main/java/project1/main.java
@@ -0,0 +1,48 @@
+package project1;
+
+import java.util.Scanner;
+
+public class main {
+
+	/*
+	 * starting point
+	 * 
+	 * Runner
+	 */
+	public static void main(String[] args) {
+
+		//String name = "LoL";
+		//System.out.print("Hello!");
+
+		// int = whole numbers
+		// int number = 120;
+
+		// wrapper class
+		// integer num = 203;
+
+//		double radius;
+//		double area;
+//
+//		radius = 20;
+//		
+//		area = radius * radius * 3.14159;
+//		
+//		System.out.println("the area is " + radius + " is " + area);
+
+		System.out.println(Math.random());
+		
+		Scanner userInput = new Scanner(System.in);
+		
+		System.out.println("Enter your name: ");
+		
+		String userName = userInput.nextLine();
+		
+		System.out.println("My name is " + userName);
+		
+		
+		
+		
+		
+	}
+
+}
